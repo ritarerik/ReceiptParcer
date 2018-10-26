581ac86ccef84066a909c66577483830d2016232
metelevamd <metelevamd@KM-DIT-N01.doronichi.com>
2018-10-26 15:03:22 +0300

    1.0

---
+++ src/CustomFilter.java
@@ -0,0 +1,26 @@
+
+import java.io.File;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Маргарита
+ */
+public class CustomFilter extends javax.swing.filechooser.FileFilter {
+        @Override
+        public boolean accept(File file) {
+            // Allow only directories, or files with ".txt" extension
+            return file.isDirectory() || file.getAbsolutePath().endsWith(".txt");
+        }
+        @Override
+        public String getDescription() {
+            // This description will be displayed in the dialog,
+            // hard-coded = ugly, should be done via I18N
+            return "Text documents (*.txt)";
+        }
+    } 
