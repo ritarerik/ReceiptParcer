71e181c607fffd9d6e79cb4075af218fe6dd297f
metelevamd <metelevamd@KM-DIT-N01.doronichi.com>
2018-10-29 15:29:15 +0300

    2.2 beta

--- src/CustomFilterXLS.java
+++ src/CustomFilterXLS.java
@@ -14,13 +14,10 @@ import java.io.File;
 public class CustomFilterXLS extends javax.swing.filechooser.FileFilter {
         @Override
         public boolean accept(File file) {
-            // Allow only directories, or files with ".txt" extension
             return file.isDirectory() || file.getAbsolutePath().endsWith(".xls");
         }
         @Override
         public String getDescription() {
-            // This description will be displayed in the dialog,
-            // hard-coded = ugly, should be done via I18N
             return "Excel file (*.xls)";
         }
     } 
