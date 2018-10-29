5ee1be4c976c9002c77693569f41ec4ad2825316
metelevamd <metelevamd@KM-DIT-N01.doronichi.com>
2018-10-29 15:29:22 +0300

    2.2 beta

--- src/CustomFilter.java
+++ src/CustomFilter.java
@@ -14,13 +14,10 @@ import java.io.File;
 public class CustomFilter extends javax.swing.filechooser.FileFilter {
         @Override
         public boolean accept(File file) {
-            // Allow only directories, or files with ".txt" extension
             return file.isDirectory() || file.getAbsolutePath().endsWith(".txt");
         }
         @Override
         public String getDescription() {
-            // This description will be displayed in the dialog,
-            // hard-coded = ugly, should be done via I18N
             return "Text documents (*.txt)";
         }
     } 
